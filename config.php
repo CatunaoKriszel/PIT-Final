c20610bc2f4f13cff6bb690474b6235911fe948d
Kriszel-Catunao <147257052+CatunaoKriszel@users.noreply.github.com>
2024-12-19 10:55:34 +0700

    config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 		$servername = "localhost";
-		$username = "root"; 
-		$password = ""; 
-		$dbname = "mydb"; 
+		$username = "angeline"; 
+		$password = "Calona@22"; 
+		$dbname = "mybook"; 
 
 		$conn = new mysqli($servername, $username, $password, $dbname);
 		if ($conn->connect_error) {    
